b5ad2454657a7098c5bf60384b209caf58359fd0
pawel10.24 <pawel10.24@vp.pl>
2023-04-05 16:59:49 +0000

    update terraform

---
+++ provider.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+  region  = "us-east-1"
+  # profile = "panda" # tylko gdy istnieje taki --profile w ~/.aws/credentials
+}
+
+# terraform
+ 
+terraform {
+  backend "s3" {
+    # bucket = "<name>-<surname>-panda-devops-core-12"
+    bucket = "pawel-wozniak-panda-devops-core-12"
+    key = "infra/terraform.state"
+    region = "us-east-1"
+  }
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.33"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
